7106510c1a33eda0451fc130f154288154925135
Mitchell <mitchellm27@gmail.com>
2022-05-10 22:21:12 -0700

    added quiz questions

---
+++ app.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+      questions: "What does a while loop do?:",
+      choices: ["Save to local storage", "Defines the data type of an object", "Loops through the duration of a parameter", ""],
+      answer: "Loops through the duration of a parameter"
+    },
+    {
+      title: "What does an If statement do?:",
+      choices: ["Loops through data", "Maps through data", "Does something under specific parameters", "compares values"],
+      answer: "Does something under specific parameters"
+    },
+    {
+      title: "Which of the following is not a common data type?:",
+      choices: ["Boolean", "string", "letters", "numbers"],
+      answer: "letters"
+    },
+    {
+      title: "Which of the following is not an operator?:",
+      choices: ["===", "_+-", "==", "&&"],
+      answer: "_+-"
+    },
+    {
+      title: "What does NaN mean:",
+      choices: ["Null and Nothing", "Not a Number", "Number acquired Negative", "Number at Node"],
+      answer: "Not a Number"
+    },
+  ];
